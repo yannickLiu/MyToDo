6967bc66f25d28291530b6a95f6a8331d3950c0a
yannick <742973911@qq.com>
2023-02-11 22:41:27 +0800

    创建prism app工程，添加MaterialDesignThems库

---
+++ PrismApp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Ioc;
+using PrismApp.Views;
+using System.Windows;
+
+namespace PrismApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ViewA>();
+            containerRegistry.RegisterForNavigation<ViewB>();
+            containerRegistry.RegisterForNavigation<ViewC>();
+        }
+    }
+}
---
+++ PrismApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace PrismApp.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            RegionManager = regionManager;
+        }
+
+        DelegateCommand<string> _command;
+        public DelegateCommand<string> Command => _command ?? (_command = new DelegateCommand<string>((arg) => 
+        {
+            RegionManager.Regions["ContentRegion"].RequestNavigate(arg);
+        }));
+
+        public IRegionManager RegionManager { get; }
+    }
+}
